b197efabe7d89f5466a6fc35d4b2a479a8d6cd71
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2023-06-09 15:12:38 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+pkg install -y xorg xfce slim slim-theme nano vim htop wget git curl librewolf firefox
+sysrc dbus_enable=YES
+sysrc slim_enable=YES
+sysrc moused_enable=YES
+sysrc hald_enable=YES
+sysrc sound_loud=YES
+sysrc snd_hda_load=YES
+echo "exec start xfce4" > ~/.xinitrc
+startx
