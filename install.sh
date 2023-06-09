c5bf0a8d01123a8fc3d21852eebc87687e0b4256
Sh@d0w <32146568+ps81frt@users.noreply.github.com>
2023-06-09 18:13:46 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
-pkg install -y xorg xfce slim slim-theme nano vim htop wget git curl librewolf firefox
+pkg install -y xorg xfce slim slim-theme nano vim htop wget git curl librewolf firefox drm-kmod
 sysrc dbus_enable=YES
 sysrc slim_enable=YES
 sysrc moused_enable=YES
 sysrc hald_enable=YES
 sysrc sound_loud=YES
 sysrc snd_hda_load=YES
-echo "exec startxfce4" > ~/.xinitrc
-cd /usr/local/share/slim/themes/ && git clone https://github.com/gocobachi/slim-freebsd-dark-theme.git
+sysrc kld_list=i915kms
+pw groupmod video -m tech || pw groupmod wheel -m tech
+echo "exec startxfce4" > /home/tech/.xinitrc
+vim /boot/loader.conf
+# Ahouter a la 1ère ligne 
+kern.vty=vt
+sed -i "1i kern.vty=vt" > /boot/loader.conf
+get https://github.com/gocobachi/slim-freebsd-dark-theme/archive/refs/heads/master.zip
+unzip -d /usr/local/share/slim/themes/
+cd /usr/local/share/slim/themes/
 sudo vim /usr/local/etc/slim.conf 
 #current_theme      default
 #current_theme       slim-freebsd-dark-theme
